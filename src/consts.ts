9e718cd175217965c4712a62bd9805c936d0011c
BertNgomsi <135256403+BertNgomsi@users.noreply.github.com>
2026-08-11 17:05:17 -0400

    Update font to Oswald for readability and remove Astro Blog title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Vanguard Wire';
+export const SITE_DESCRIPTION = 'The definitive digital ledger for Black progressive politics, civil rights, and accountability.';
